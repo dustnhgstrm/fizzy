277aec4376087411f9149afb08c8ea0cf1118703
Dustin Hagstrom <dustin.l.hagstrom@gmail.com>
2021-02-22 14:59:06 -0500

    wrote fizzbuzz function

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function fizzy() {
+  const fizz = "Fizz"
+  const buzz = "Buzz"
+  const fBuzz = "FizzBuz"
+  if (num % 3 !== 0 && num % 5 !== 0) {
+    return num;
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return fBuzz;
+  } else if ( num % 3 === 0 && num % 5 !== 0) {
+    return fizz;
+  } else return buzz;  
 
 
 
